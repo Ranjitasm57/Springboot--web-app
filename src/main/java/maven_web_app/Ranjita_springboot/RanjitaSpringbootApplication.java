d35d671626154adb1385375479f417ea7ac51683
Ranjitasm57 <ranjitasm57@gmail.com>
2024-07-01 11:30:48 +0530

    added a files

---
+++ src/main/java/maven_web_app/Ranjita_springboot/RanjitaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package maven_web_app.Ranjita_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RanjitaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RanjitaSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/maven_web_app/Ranjita_springboot/RanjitaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package maven_web_app.Ranjita_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RanjitaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
